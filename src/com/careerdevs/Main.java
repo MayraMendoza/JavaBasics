1e225cfc6b47d17f85425b3e88922aff8561b88f
MayraMendoza <mayra601m@gmail.com>
2022-01-03 21:25:27 -0500

    3/3 completed

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,19 +3,25 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	String firstName = "Mayra ";
-    String lastName = "Mendoza";
-    String fullName = firstName + lastName;
-    System.out.println(fullName);
-    System.out.println(fullName("Mayra", "Mendoza"));
-
+        String firstName = "Mayra ";
+        String lastName = "Mendoza";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        System.out.println(fullName("Mayra", "Mendoza"));
+        System.out.println(NameGenerator.generateFullName("Mayra", "Mendoza"));
     }
 
-    public static String fullName(String first, String last)
-    {
+    public static String fullName(String first, String last) {
         String fullName = first + " " + last;
 
         return fullName;
+    }
+}
 
+class NameGenerator {
+    public static String generateFullName ( String first, String last)
+    {
+        String fullName = first + " " + last;
+        return fullName;
     }
 }
