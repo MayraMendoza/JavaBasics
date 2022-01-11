82ee552ae3b50007e5aff99d83a97e0861bd759c
MayraMendoza <mayra601m@gmail.com>
2022-01-10 22:20:21 -0500

    Car instances

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,28 @@
+package com.careerdevs;
+
+public class Car {
+    public String make;
+    public String model;
+    public int milage;
+    public float gasTankPercent;
+
+
+    public Car(String make, String model, int milage, float gasTankPercent ){
+        this.make = make;
+        this.model = model;
+        this.milage = milage;
+        this.gasTankPercent = gasTankPercent;
+
+
+    }
+
+    public String carInfo(){
+
+        return "Make:" + this.make +", "+ "Model:"+ this.model;
+
+
+
+    }
+
+
+}
