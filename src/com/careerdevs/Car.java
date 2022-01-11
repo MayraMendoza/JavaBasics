09f507b6733513939ad0c8edf08259b45d7f268a
MayraMendoza <mayra601m@gmail.com>
2022-01-10 22:38:00 -0500

    Car fields set to private & toString() method added.

--- src/com/careerdevs/Car.java
+++ src/com/careerdevs/Car.java
@@ -1,10 +1,10 @@
 package com.careerdevs;
 
 public class Car {
-    public String make;
-    public String model;
-    public int milage;
-    public float gasTankPercent;
+    private String make;
+    private String model;
+    private int milage;
+    private float gasTankPercent;
 
 
     public Car(String make, String model, int milage, float gasTankPercent ){
@@ -16,10 +16,44 @@ public class Car {
 
     }
 
+    public String toString(){
+        return "Make:" + this.make +", "+ "Model:"+ this.model +", "+ "Milage:"+ this.milage +", "+ "gasTankPercent:"+ this.gasTankPercent;
+    }
+
     public String carInfo(){
 
         return "Make:" + this.make +", "+ "Model:"+ this.model;
     }
 
+    public String getMake() {
+        return make;
+    }
 
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMilage() {
+        return milage;
+    }
+
+    public void setMilage(int milage) {
+        this.milage = milage;
+    }
+
+    public float getGasTankPercent() {
+        return gasTankPercent;
+    }
+
+    public void setGasTankPercent(float gasTankPercent) {
+        this.gasTankPercent = gasTankPercent;
+    }
 }
