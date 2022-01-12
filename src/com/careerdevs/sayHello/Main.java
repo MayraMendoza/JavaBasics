00a4cb65fcf2d867e3592da3ff104d24557132d4
MayraMendoza <mayra601m@gmail.com>
2022-01-11 22:05:13 -0500

    access private fields with getters &  print car make, model, mileage ect with toString method.

---
+++ src/com/careerdevs/sayHello/Main.java
@@ -0,0 +1,38 @@
+package com.careerdevs.sayHello;
+
+import com.careerdevs.NameGenerator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        // A
+        Scanner scanner = new Scanner(System.in);
+        whatIsYourName();
+        String name = scanner.nextLine();
+        System.out.println("Hello, nice to meet you "+ name);
+
+        //B
+        System.out.print("What is your first name? ");
+        String firstName = scanner.next();
+        System.out.print("What is your last name? ");
+        String lastName = scanner.next();
+
+
+        whatIsYourFullName(firstName, lastName);
+
+
+    }
+
+    public static void whatIsYourName (){
+        System.out.print("What is your Name? ");
+    }
+    public static void whatIsYourFullName (String first ,String last){
+        String fullName = NameGenerator.generateFullName(first,last);
+        System.out.println("Its nice to formally meet you " + fullName);
+
+
+
+    }
+
+}
