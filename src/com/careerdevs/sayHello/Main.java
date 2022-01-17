97b85c5b868ce00ac9df8d05aad778a534e55aff
MayraMendoza <mayra601m@gmail.com>
2022-01-17 13:32:06 -0500

    sayHello strings + ints

--- src/com/careerdevs/sayHello/Main.java
+++ src/com/careerdevs/sayHello/Main.java
@@ -11,40 +11,34 @@ public class Main {
 //    // method.
 //    public static Scanner = new Scanner(System.in);
     public static void main(String[] args){
-//        // A
+        // A
         Scanner scanner = new Scanner(System.in);
 
-//        whatIsYourName();
+        whatIsYourName();
 //        String name = scanner.nextLine();
 //        System.out.println("Hello, nice to meet you "+ name);
-//
-//        //B
-//        System.out.print("What is your first name? ");
-//        String firstName = scanner.next();
-//        System.out.print("What is your last name? ");
-//        String lastName = scanner.next();
-//
-//
-//        whatIsYourFullName(firstName, lastName);
-//
+
+        //B
+        System.out.print("What is your first name? ");
+        String firstName = scanner.next();
+        System.out.print("What is your last name? ");
+        String lastName = scanner.next();
+        scanner.nextLine();
+
+
+        whatIsYourFullName(firstName, lastName);
+
         // loop?
         ArrayList<String> questions = new ArrayList<String>();
-        questions.add("what is your name? ");
-        questions.add("how old are you? ");
-        questions.add("what is your favorite color? ");
-        questions.add("what city are you from? ");
-        questions.add("whats your favorite holiday? ");
+        questions.add("what is your full name? ");
+        questions.add("what is your date of birth ex. 01/21/21? ");
+        questions.add("what city were you born in? ");
+        questions.add("what country were you born in? ");
+        questions.add("what is your mothers maiden name? ");
 
 
        ArrayList<String> userAnswers = new ArrayList<String>();
-
-       //System.out.println("To exit enter N to continue please enter y");
-        //            stop = scanner.next();
-        //            if (stop.equalsIgnoreCase("y")) {
-
-//        String stop= "y";
-//        for(int i= 0; ;){
-
+// used scanner with int.
         int stop = 1;
         int z =0;
             while (stop == 1 && z< questions.size()) {
